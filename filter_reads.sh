ada2ab1e5c59db513b7c7aa9a905ff2042e629dd
Ewan Xiong <53613810+xyifan97@users.noreply.github.com>
2022-04-09 19:10:26 +0800

    Update filter_reads.sh

--- filter_reads.sh
+++ filter_reads.sh
@@ -4,12 +4,26 @@
 # 3. pair reads not alignment on the same chromosome
 # 4. reads mapping on ChrM
 
+###
+samtools index test.bam
+samtools flagstat test.bam > test.raw.stat
+
 ### use sambamba/samtools remove duplicate 
 sambamba markdup -r -t 64 test.bam test.rmdup.bam
+### error sambamba-markdup: Cannot open file `/tmp/sambamba-pid96081-markdup-saro/PairedEndsInfoisfy1' in mode `w+' (Too many open files)
+### add --overflow-list-size 600000
+samtools index test.rmdup.bam
+samtolls flagstat test.rmdup.bam > test.rmdup.stat
 
 ### use IGV virsulizatipn reads 
-samtools index test_sorted.bam
-### get test_sorted.bam.bai index  ues bam and bai for IGV plot
 
 ### samtools rm unproper pair mapping, low quality reads and chrM reads
-samtools view -f 2 -q 30 test.rmdup.bam|grep -v chrM
+samtools view -f 2 -q 30 test.rmdup.bam|grep -v chrM |samtools sort -O bam -@ 64 -o test.last.bam -  
+
+samtools index test.last.bam
+samtools flagstat test.last.stat
+
+
+
+
+
