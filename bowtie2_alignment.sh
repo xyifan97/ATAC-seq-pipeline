e572ffdf7642326cdea1b20c01949152903cb86c
Ewan Xiong <53613810+xyifan97@users.noreply.github.com>
2022-04-09 10:24:27 +0800

    Update bowtie2_alignment.sh

--- bowtie2_alignment.sh
+++ bowtie2_alignment.sh
@@ -1,5 +1,5 @@
 ###
-/Date/biosoft/bowtie2-2.3.4-linux-x86_64/bowtie2 -x ref_genome_index -1 fq1.gz -2 fq2.gz -p threads_num |samtools sort -@ 20 -O bam -o fq.bam -
+/Date/biosoft/bowtie2-2.3.4-linux-x86_64/bowtie2 -x ref_genome_index -1 fq1.gz -2 fq2.gz -p threads_num |samtools sort -@ 64 -O bam -o fq.bam -
 
 
 ###
