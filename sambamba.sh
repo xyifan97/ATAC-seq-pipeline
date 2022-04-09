75381993f9937b211f84edee7527cd864909fb52
Ewan Xiong <53613810+xyifan97@users.noreply.github.com>
2022-04-09 10:28:46 +0800

    Update sambamba.sh

--- sambamba.sh
+++ sambamba.sh
@@ -1,2 +1,8 @@
 ### use sambamba remove duplicate 
 sambamba markdup -r -t 64 test.bam test.rmdup.bam
+
+### use IGV virsulizatipn reads 
+samtools index test_sorted.bam
+### get test_sorted.bam.bai index
+
+### ues bam and bai for IGV plot
